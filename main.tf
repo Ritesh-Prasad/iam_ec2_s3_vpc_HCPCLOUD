a8f6c2b614a62dfbc337b33f3f72e88a626ba3a1
Ritesh-Prasad <ritesh010996@gmail.com>
2026-02-13 12:55:36 +0530

    2 commit

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+module "iam-user" {
+  source = "./modules/iam"
+  
+}
+
+module "ec2-server" {
+  source = "./modules/ec2"
+  
+}
+
+module "s3-bucket" {
+  source = "./modules/s3"
+}
+
+module "vpc-network" {
+  source = "./modules/vpc"
+}
